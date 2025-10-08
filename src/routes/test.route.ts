f2887a28e82b6f512d471a0d93c7a551ad153827
toiladat <tdat9663@gmail.com>
2025-10-08 18:18:33 +0700

    test cicd

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -3,7 +3,7 @@ import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 export default async function testRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
   fastify.get('/', async (request, reply) => {
     reply.send({
-      message: 'Hello from test route 5!'
+      message: 'anh dat cua ems'
     })
   })
 }
